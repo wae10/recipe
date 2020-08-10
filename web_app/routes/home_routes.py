318233c00252f59c4268374c4f393988eb6e5e6c
wae10 <59625812+wae10@users.noreply.github.com>
2020-08-10 02:27:05 -0500

    web app test

---
+++ web_app/routes/home_routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template, redirect, request, flash
+
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route("/")
+def index():
+    print("VISITED THE HOME PAGE")
+    #return "Welcome Home (TODO)"
+    return render_template("home.html")
